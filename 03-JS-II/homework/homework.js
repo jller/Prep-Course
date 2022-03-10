4691323116e23508b96c3da3064dc8637dce1feb
jller <jller@protonmail.com>
2022-03-09 19:00:35 -0500

    Avance

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -36,9 +36,9 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-  if(idioma==aleman) return 'Guten Tag!';
-  if(idioma==mandarin) return 'Ni Hao!';
-  if(idioma==ingles) return 'Hello!';
+  if(idioma=='aleman') return 'Guten Tag!';
+  if(idioma=='mandarin') return 'Ni Hao!';
+  if(idioma=='ingles') return 'Hello!';
   else return 'Hola!';
 }
 
@@ -50,10 +50,10 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
-  if(color==blue) return 'This is blue';
-  if(color==red) return 'This is red';
-  if(color==green) return 'This is green';
-  if(color==orange) return 'This is orange'
+  if(color=='blue') return 'This is blue';
+  if(color=='red') return 'This is red';
+  if(color=='green') return 'This is green';
+  if(color=='orange') return 'This is orange'
   else return 'Color not found';
 }
 
@@ -61,14 +61,16 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(numero==5 || numero==10) return 'true';
-  else return 'false';
+  if(numero==5 || numero==10) return true;
+  else return false;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero<50 && numero>20) return true;
+  else return false;
 }
 
 function esEntero(numero) {
@@ -79,8 +81,8 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if ((numero - Math.floor(numero))==0) return 'true';
-  else return 'false';
+  if ((numero - Math.floor(numero))==0) return true;
+  else return false;
 }
 
 function fizzBuzz(numero) {
@@ -88,9 +90,10 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if(numero%3==0 && numero%5==0)  return 'fizzbuzz';
   if(numero%3==0) return 'fizz';
   if(numero%5==0) return 'buzz';
-  if(numero%3==0 && numero%5==0)  return 'fizzbuzz';
+  
   else return numero;
 }
 
